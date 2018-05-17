2fa014cfb8085511969317e274d7f9d43f3b8d39
HarrisonMH <crooner99@gmail.com>
2018-05-17 14:23:52 -0700

    Fixed more semicolons

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
 const crypto = require('crypto');
 const mysql = require('mysql');
 const nodemailer = require('nodemailer');
-const email = require('./send_email.js')
+const email = require('./send_email.js');
 
 var app = express();
 const port = process.env.PORT || 8080;
@@ -41,14 +41,12 @@ var con = mysql.createConnection({
     port: credentials.port
 });
 var Accs = [];
-<<<<<<< HEAD
-=======
 
 var send_mail = () => {   
-    options = email.mailOptions
-    options.to = 'viktor.sheverdin@gmail.com'
-    options.subject = 'Test email from Sb app'
-    options.text = 'OK! It actually works!'
+    options = email.mailOptions;
+    options.to = 'viktor.sheverdin@gmail.com';
+    options.subject = 'Test email from Sb app';
+    options.text = 'OK! It actually works!';
     console.log(options);
     email.send_email(options);
 
@@ -60,7 +58,7 @@ var send_mail = () => {
     //         console.log('Email sent: ', info.response);
     //     }
     // });
-}
+};
 
 // transporter.sendMail(mailOptions, function(error, info){
 //   if (error) {
@@ -69,7 +67,6 @@ var send_mail = () => {
 //     console.log('Email sent: ' + info.response);
 //   }
 // });
->>>>>>> 7f35f9090bf45a3afc23cab2c1755cde2b9c83b5
 
 var LoadAccfile = () => {
     return new Promise(resolve => {
@@ -380,7 +377,7 @@ app.post('/storeuserdata', (request, response) => {
     }
     console.log(account);
     fs.writeFileSync('accounts.json', JSON.stringify(account));*/
-    last_save = request.body.location
+    last_save = request.body.location;
     checkLocations(logged_in.username, request.body.location).then(res => {
         addLocations(logged_in.username, request.body.location);
     }, rej => { console.log('failed');
@@ -399,7 +396,7 @@ app.post('/favdata', (request, response) => {
             console.log(saved_loc[i].location_id);
             displaySaved += `<div id=s${i} class="favItems"><a onclick="getMap(${saved_loc[i].location_id})"> ${saved_loc[i].location_id}</a></div>`;
         }
-         displaySaved += `<div id=s${saved_loc.length} class="favItems"><a onclick="getMap(${last_save})"> ${last_save}</a></div>`
+         displaySaved += `<div id=s${saved_loc.length} class="favItems"><a onclick="getMap(${last_save})"> ${last_save}</a></div>`;
 
 
         current_ip.request_coodrs().then((response1) => {
