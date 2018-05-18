e0dacf9b8c1fcbc0901038de245079cddb7868e3
HarrisonMH <crooner99@gmail.com>
2018-05-18 01:12:25 -0700

    Unit testing

--- server.js
+++ server.js
@@ -434,8 +434,12 @@ var server = app.listen(port, () => {
 
 
 module.exports = {
+    send_mail,
     UserNameCheck,
     PasswordCheck,
     LoginCheck,
-    server
+    server,
+    LoadAccfile,
+    loadUserdata,
+    checkLocations
 };
